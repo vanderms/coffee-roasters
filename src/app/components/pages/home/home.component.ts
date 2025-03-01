424520d979c23488986bc68c0a3847916c35dbb6
Vander Santos <vanderms.84@gmail.com>
2025-03-01 12:32:45 -0300

    partial #5: implements our collection section

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+type CollectionCard = {
+  src: string;
+  title: string;
+  text: string;
+};
+
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  protected readonly collection: CollectionCard[] = [
+    {
+      src: 'assets/stock/collection/gran-expresso.png',
+      title: 'Gran Espresso',
+      text: ' Light and flavorful blend with cocoa and black pepper for an intense experience',
+    },
+    {
+      src: 'assets/stock/collection/planalto.png',
+      title: 'Planalto',
+      text: 'Brazilian dark roast with rich and velvety body, and hints of fruits and nuts',
+    },
+    {
+      src: 'assets/stock/collection/picollo.png',
+      title: 'Picollo',
+      text: 'Mild and smooth blend featuring notes of toasted almond and dried cherry ',
+    },
+    {
+      src: 'assets/stock/collection/danche.png',
+      title: 'Danche',
+      text: 'Ethiopian hand-harvested blend densely packed with vibrant fruit notes',
+    },
+  ];
+}
