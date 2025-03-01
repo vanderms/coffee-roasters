a5a5692a92a0f37adaebe4174c55cebd559470ee
Vander Santos <vanderms.84@gmail.com>
2025-02-28 23:55:23 -0300

    wip #5: added hero images

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Coffeeroasters: Homepage',
+    loadComponent: () => {
+      return import('../app/components/pages/home/home.component').then(
+        (m) => m.HomeComponent,
+      );
+    },
+  },
+];
